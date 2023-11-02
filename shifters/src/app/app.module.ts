50ff173357748284ec4de8d01b5954ec0df8f38a
DickySosa <dicky5217@gmail.com>
2023-11-02 12:54:53 -0600

    Adding autentication pages

--- shifters/src/app/app.module.ts
+++ shifters/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SinginComponent } from './pages/autentication/singin/singin.component';
+import { RegisterComponent } from './pages/autentication/register/register.component';
+import { VerificationCodeComponent } from './pages/autentication/verification-code/verification-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinginComponent,
+    RegisterComponent,
+    VerificationCodeComponent
   ],
   imports: [
     BrowserModule
